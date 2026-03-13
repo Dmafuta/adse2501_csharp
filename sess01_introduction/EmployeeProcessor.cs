45af761ca51a114a441288d126601cfa346c71de
Dmafuta <dmafuta07@gmail.com>
2026-03-13 14:33:44 +0300

    Learning on various method referencing

---
+++ sess01_introduction/EmployeeProcessor.cs
@@ -0,0 +1,14 @@
+﻿namespace sess01_introduction
+{
+    public class EmployeeProcessor
+    {
+        public delegate void EmployeeProcessedHandler(char gender);
+        public event EmployeeProcessedHandler? EmployeeProcessed;
+
+        public void Process(SampleEmployeeXMLDocumentation emp)
+        {
+            char gender = emp.GetGender();
+            EmployeeProcessed?.Invoke(gender);
+        }
+    }
+}
