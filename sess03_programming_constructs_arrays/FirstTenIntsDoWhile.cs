255acc8bce95096c1895949b5e9ccbcef8ed24bd
Dmafuta <dmafuta07@gmail.com>
2026-03-30 09:37:54 +0300

    First ten int do while

---
+++ sess03_programming_constructs_arrays/FirstTenIntsDoWhile.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+namespace sess03_programming_constructs_arrays
+{
+    /// <summary>
+    /// Program to display the first 10 integers 
+    /// </summary>
+    public class FirstTenIntsDoWhile
+    {
+        static void Main(string[] args)
+        {
+            uint n = 1;
+            do
+            {
+                Console.WriteLine(n);
+                n++;
+            }while (n <= 10) ;
+        }
+    }
+}
